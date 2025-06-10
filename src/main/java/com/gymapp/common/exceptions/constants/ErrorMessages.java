5fd84461d7d27307b3a313b80d074c54e91a1694
daniel garcia <bloodgigameta@gmail.com>
2025-06-10 14:26:50 -0600

    refactor(exceptions): delete class ErrorCodes, create class ErrorMessages

---
+++ src/main/java/com/gymapp/common/exceptions/constants/ErrorMessages.java
@@ -0,0 +1,12 @@
+package com.gymapp.common.exceptions.constants;
+
+public class ErrorMessages {
+  public static final String BAD_REQUEST = "The request is invalid or malformed.";
+  public static final String UNAUTHORIZED = "Authentication is required to access this resource.";
+  public static final String FORBIDDEN = "You do not have permission to access this resource.";
+  public static final String BUSINESS_VALIDATION = "The operation failed due to a business rule violation.";
+  public static final String EXTERNAL_SERVICE_ERROR = "An error occurred while communicating with an external service.";
+  public static final String INTERNAL_SERVER_ERROR = "An unexpected error occurred. Please contact support.";
+
+  private ErrorMessages() {}
+}
